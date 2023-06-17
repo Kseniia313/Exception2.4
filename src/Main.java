7f2a05056086c273bbf3c88a0ce561e30d626ae5
Kseniia313 <kshome@mail.ru>
2023-06-17 22:54:20 +0300

    Первый коммит

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,9 @@
 public class Main {
-public static void main (String[] args){
-    try {
-        Validator.validateWithRegex("login", "password", "passwordconfirm");
-    }
-    catch (WrongLoginException | WrongPasswordException ex){
-        System.out.println(ex.getMessage());
-    }
+    public static void main(String[] args) {
+        try {
+            Validator.validateWithRegex("login", "password", "passwordconfirm");
+        } catch (WrongLoginException | WrongPasswordException ex) {
+            System.out.println(ex.getMessage());
+        }
     }
 }
